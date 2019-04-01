22b79a7d11ed87d6e33a619485d14815fa5cda94
cjSound <caojie@chalco-steering.com>
2019-04-01 11:42:17 +0800

    ui   store

--- src/config/service.js
+++ src/config/service.js
@@ -1,8 +1,8 @@
-var DEV_URL='http://192.168.31.251:8099/';
-var DEV_FILE='http://192.168.31.251:8080/photo-service/'
+var DEV_URL='http://192.168.11.90:8099/photo-service/';
+var DEV_FILE='http://192.168.11.90/photo-service/'
 
-var PRODUCT_URL='https://www.cjsound.top/';
-var PRODUCT_FILE='https://www.cjsound.top/';
+var PRODUCT_URL='https://www.cjsound.top/photo-service/';
+var PRODUCT_FILE='https://www.cjsound.top/photo-service/';
 
 var URL =process.env.NODE_ENV=='production'?PRODUCT_URL:DEV_URL;
 var FILE_URL =process.env.NODE_ENV=='production'?PRODUCT_FILE:DEV_FILE;
